07d069b518c808e9f7a9a81cc0c047aa0ce3adf5
MiekeNap <84043094+MiekeNap@users.noreply.github.com>
2021-06-07 22:39:28 +0200

    Add files via upload

---
+++ Opdracht_016_functions/script.js
@@ -0,0 +1,26 @@
+function makePizza (){
+    console.log("maak je pizza deeg met bloem en water");
+    console.log("rol je pizza deeg uit tot een mooie ronde platte bodem");
+    console.log("voeg je tomatensaus en andere topppings toe");
+    console.log("bak je pizza in de oven");
+};
+//you need to call the function before it shows
+makePizza();
+makePizza();
+makePizza();
+
+function makeSushi (){
+    console.log("kook de rijst voor je sushi");
+    console.log("snijd je groenten en andere ingredienten in lange dunne reepjes");
+    console.log("rol je rijst en andere ingredienten op tot sushi");
+    console.log("snijdt de lange sushi rollen in dunne plakken: je sushi!");
+};
+
+makeSushi();
+
+//3 meals of Sushi and 2 pizza's
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
